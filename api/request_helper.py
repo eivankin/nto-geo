b922f95cbac36f2cfc7cd908563d064c4d51beea
Evgenij Ivankin <e.ivankin@innopolis.university>
2023-04-19 22:26:30 +0300

    revert helper & add data saving

--- api/request_helper.py
+++ api/request_helper.py
@@ -3,7 +3,7 @@ from typing import Any
 from pydantic import parse_obj_as, BaseModel
 from requests import Response
 
-from api_models import CatalogResponse, ObjectResponse
+from api.api_models import CatalogResponse, ObjectResponse
 from requests import get, post
 
 
